1e0e5851b3d6b4a918a80477e6b6f7199556353b
Bayakan <spring.boot@hotmail.com>
2024-06-03 17:23:17 +0300

    Spring Boot. 2.1 Conditional & Autoconfigure

---
+++ src/main/java/com/dmdev/springboot/lesson/config/ApplicationConfiguration.java
@@ -0,0 +1,21 @@
+package com.dmdev.springboot.lesson.config;
+
+import com.dmdev.springboot.lesson.conditional.FirstConditional;
+import jakarta.annotation.PostConstruct;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Conditional;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Conditional(FirstConditional.class)
+@Profile("dev")
+@Configuration
+public class ApplicationConfiguration {
+    private static final Logger log = LoggerFactory.getLogger(ApplicationConfiguration.class);
+    @PostConstruct
+    public void init() {
+        log.warn("app is loaded!!!");
+
+    }
+}
