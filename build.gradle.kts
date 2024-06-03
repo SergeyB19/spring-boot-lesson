9ecbb13af2cb8b0d81c1f4f4caee14dd61c52276
Bayakan <spring.boot@hotmail.com>
2024-06-03 12:52:11 +0300

    Spring Boot. 1.1 Первое приложение

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,8 @@
 plugins {
     id("java")
+    id("org.springframework.boot") version "3.2.2"
+    id("io.spring.dependency-management") version "1.1.5"
+
 }
 
 group = "com.dmdev"
@@ -12,6 +15,9 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation("org.springframework.boot:spring-boot-starter")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
 }
 
 tasks.test {
