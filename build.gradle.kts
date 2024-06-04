fe16015b04e79ffc0c909765bc9969064f298f98
Bayakan <spring.boot@hotmail.com>
2024-06-04 09:24:12 +0300

    Spring Boot. 3.1 Lombok

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,11 @@
 plugins {
     id("java")
-    id("org.springframework.boot") version "3.2.2"
+    id("org.springframework.boot") version "3.1.4"
     id("io.spring.dependency-management") version "1.1.5"
+/*
+    kotlin("plugin.lombok") version "2.0.0"
+    id("io.freefair.lombok") version "8.1.0"*/
+
 
 }
 
@@ -16,10 +20,15 @@ dependencies {
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     implementation("org.springframework.boot:spring-boot-starter")
-/*    implementation("org.springframework.boot:spring-boot-starter-web")
-    implementation("org.springframework.boot:spring-boot-starter-actuator")*/
+    /*    implementation("org.springframework.boot:spring-boot-starter-web")
+        implementation("org.springframework.boot:spring-boot-starter-actuator")*/
+    compileOnly("org.projectlombok:lombok:1.18.30")
+    annotationProcessor("org.projectlombok:lombok:1.18.30")
+
 }
+
 
 tasks.test {
     useJUnitPlatform()
+
 }
--- src/main/java/com/dmdev/springboot/lesson/config/ApplicationConfiguration.java
+++ src/main/java/com/dmdev/springboot/lesson/config/ApplicationConfiguration.java
@@ -2,17 +2,16 @@ package com.dmdev.springboot.lesson.config;
 
 import com.dmdev.springboot.lesson.conditional.FirstConditional;
 import jakarta.annotation.PostConstruct;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Conditional;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-
+@Slf4j
 @Conditional(FirstConditional.class)
-@Profile("dev")
+//@Profile("dev")
 @Configuration
 public class ApplicationConfiguration {
-    private static final Logger log = LoggerFactory.getLogger(ApplicationConfiguration.class);
+//    private static final Logger log = LoggerFactory.getLogger(ApplicationConfiguration.class);
     @PostConstruct
     public void init() {
         log.warn("app is loaded!!!");
