a01979c8c4efbe35d8277efe941aea1cebad7a48
Bayakan <spring.boot@hotmail.com>
2024-06-04 09:54:05 +0300

    Spring Boot. 4.1 Test starter

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,6 +24,7 @@ dependencies {
         implementation("org.springframework.boot:spring-boot-starter-actuator")*/
     compileOnly("org.projectlombok:lombok:1.18.30")
     annotationProcessor("org.projectlombok:lombok:1.18.30")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
 
 }
 
